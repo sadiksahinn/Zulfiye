8b569f9976b3f2b316b5313c28d802989b54e086
Sadık Şahin <sadiksahin18@gmail.com>
2026-05-22 19:30:19 +0300

    Add register page

---
+++ app/register/page.tsx
@@ -0,0 +1,9 @@
+export default function RegisterPage() {
+  return (
+    <main className="min-h-screen flex items-center justify-center">
+      <h1 className="text-3xl font-bold">
+        Kayıt Sayfası Yakında
+      </h1>
+    </main>
+  );
+}
